05af263bfb10a0359fb91a9b1bc57f59c1e067ae
kito-06 <44410072+kito-06@users.noreply.github.com>
2018-10-23 12:58:15 -0400

    Add files via upload

---
+++ scoreLabServer.js
@@ -0,0 +1,10 @@
+/**
+ * Author: Kevin
+ * 
+ */
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Kevin loves contributing to ScoreLab.');
+}).listen(8080);
